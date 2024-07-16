8c8c78fcf5b1e6ca019904bad0f91aee0fac35cb
MIke Bush <mike.bush@valtech.com>
2024-07-16 17:59:20 -0400

    Temp working

--- app/src/main.c
+++ app/src/main.c
@@ -1,16 +1,23 @@
 #include "ss_bt.h"
+#include "bme280.h"
 
 #include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_REGISTER(main, LOG_LEVEL_INF);
 
 int main(void)
 {
-
-  init_ss_bt();
+  int res = 1000;
+  //en_coded_adv();
+  bme280_init();
 
   while (1)
   {
-    k_msleep(10000-106);
-
-    ss_bt_update(1000,98);
+    k_msleep(1000);
+    bme280_read();
+    //ss_bt_update(res,98);
+    res++;
+    LOG_INF("main");
   }
 }
--- app/src/ss_bt.h
+++ app/src/ss_bt.h
@@ -3,7 +3,18 @@
 
 #include <stdint.h>
 
+#define MANUFACTURER_ID 0x0877
+#define SUBTYPE 0x55
+
+#define MANUFACTURER_ID_LSB ((MANUFACTURER_ID >> 0) & 0xFF)
+#define MANUFACTURER_ID_MSB ((MANUFACTURER_ID >> 8) & 0xFF)
+
+#define DEVICE_NAME CONFIG_BT_DEVICE_NAME
+#define DEVICE_NAME_LEN (sizeof(DEVICE_NAME) - 1)
+#define TX_RATE ((CONFIG_BT_TX_RATE * 16) / 10)
+
 void init_ss_bt(void);
 void ss_bt_update(int32_t impediance, int8_t batt);
+void en_coded_adv(void);
 
 #endif
